ee6741b8d5f42745556179d40dd5a77ccd03bab9
louhz <hlou8@wisc.edu>
2023-03-10 18:51:26 +0800

    update psnr

--- utils/metrics.py
+++ utils/metrics.py
@@ -13,9 +13,16 @@ def mae(image_pred, image_gt):
     value=torch.abs(image_pred-image_gt)
     return  torch.mean(value)
 
-def psnr(image_pred, image_gt, valid_mask=None, reduction='mean'):
-    return -10*torch.log10(mse(image_pred, image_gt, valid_mask, reduction))
+#def psnr(image_pred, image_gt, valid_mask=None, reduction='mean'):
+#    return -10*torch.log10(mse(image_pred, image_gt, valid_mask, reduction))
 
+def psnr(image_pred, image_gt, valid_mask=None, reduction='mean'):
+    value = 20*torch.log10(1/torch.sqrt(mse(image_pred, image_gt, valid_mask, reduction)))
+    if reduction == 'mean':
+        return torch.mean(value)
+    return value
+
+    
 def ssim(image_pred, image_gt, reduction='mean'):
     """
     image_pred and image_gt: (3, H, W)
